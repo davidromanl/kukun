e6f7c8dd0cb902fb0c5b518cc313347d7746c011
David <roman.david@gmail.com>
2022-02-18 11:44:29 -0500

    master

---
+++ src/JSONdata.js
@@ -0,0 +1,296 @@
+const data = [
+  { 
+    id:  1,
+    nombre: "Alabama",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id:  2,
+    nombre: "Alaska",
+    costo: 410,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id:  3,
+    nombre: "Arizona",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id:  4,
+    nombre: "Arkansas",
+    costo: 310,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id:  5,
+    nombre: "California",
+    costo: 810,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id:  6,
+    nombre: "Carolina",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id:  7,
+    nombre: "Carolina",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id:  8,
+    nombre: "Colorado",
+    costo: 510,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id:  9,
+    nombre: "Connecticut",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 10,
+    nombre: "Dakota",
+    costo: 610,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 11,
+    nombre: "Dakota",
+    costo: 410,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 12,
+    nombre: "Delaware",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 13,
+    nombre: "Florida",
+    costo: 110,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 14,
+    nombre: "Georgia",
+    costo: 130,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 15,
+    nombre: "Hawái9​",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 16,
+    nombre: "Moku",
+    costo: 480,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 17,
+    nombre: "Idaho",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 18,
+    nombre: "Illinois",
+    costo: 150,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 19,
+    nombre: "Indiana",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 20,
+    nombre: "Iowa",
+    costo: 210,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 21,
+    nombre: "Kansas",
+    costo: 700,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 22,
+    nombre: "Kentucky",
+    costo: 410,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 23,
+    nombre: "Luisiana",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 24,
+    nombre: "État",
+    costo: 610,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 25,
+    nombre: "Maine",
+    costo: 610,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 26,
+    nombre: "Maryland",
+    costo: 710,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 27,
+    nombre: "Massachusetts",
+    costo: 800,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 28,
+    nombre: "Míchigan13​",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 29,
+    nombre: "Minnesota",
+    costo: 250,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 30,
+    nombre: "Misisipi​",
+    costo: 110,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 31,
+    nombre: "Misuri​",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 32,
+    nombre: "Montana",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 33,
+    nombre: "Nebraska",
+    costo: 500,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 34,
+    nombre: "Nevada",
+    costo: 210,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 35,
+    nombre: "Estado",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 36,
+    nombre: "Ohio",
+    costo: 310,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 37,
+    nombre: "Oklahoma",
+    costo: 410,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 38,
+    nombre: "Oregón​",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 39,
+    nombre: "Pensilvania​",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 40,
+    nombre: "Rhode",
+    costo: 710,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 41,
+    nombre: "Tennessee",
+    costo: 310,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 42,
+    nombre: "Texas",
+    costo: 410,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 43,
+    nombre: "Utah",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 44,
+    nombre: "Vermont",
+    costo: 310,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 45,
+    nombre: "Virginia",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 46,
+    nombre: "Virginia",
+    costo: 510,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 47,
+    nombre: "Washington",
+    costo: 510,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 48,
+    nombre: "Wisconsin",
+    costo: 10,
+    url: "https://app.mykukun.com/estimator/kitchen-renovation-cost/texas"
+  },
+  { 
+    id: 49,
+    nombre: "Wyoming",
+    costo: 710
+  },
+];
+export default data;
